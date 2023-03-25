1efcf3f820a6cbb978c3097f0f551ff17f2c026b
prnth <pranith_h@yahoo.com.sg>
2023-03-25 21:05:56 +0530

    updated session

--- util/auth.js
+++ util/auth.js
@@ -3,10 +3,10 @@ import { withIronSessionApiRoute, withIronSessionSsr } from "iron-session/next";
 
 const sessionOptions = {
   password: "complex_password_at_least_32_characters_long",
-  cookieName: "myapp_cookiename",
+  cookieName: "dragd_session",
   // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
   cookieOptions: {
-    secure: process.env.NODE_ENV === "production",
+    secure: false, // process.env.NODE_ENV === "production",
   },
 };
 
