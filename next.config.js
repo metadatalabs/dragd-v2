92a61ee5b6fcbd7e4cdc89b656fc9a55ea176f4e
prnth <pranith_h@yahoo.com.sg>
2023-04-07 06:31:25 +0530

    oops

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
   trailingSlash: true,
-  assetPrefix: process.env.APP_MODE === 'STATIC' ? './' : '',
+  assetPrefix: process.env.APP_MODE === "static" ? "./" : "",
   env: {
     NEXT_PUBLIC_MINTER_ADDRESS: "0x88289ac519eFb1cba5F522970E63264a969BeD06",
-  }
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
