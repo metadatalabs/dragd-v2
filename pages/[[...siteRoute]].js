51924e64956795fe1640875073a88778d2e5723b
prnth <pranith_h@yahoo.com.sg>
2023-04-22 07:59:16 +0530

    fix login?

--- pages/[[...siteRoute]].js
+++ pages/[[...siteRoute]].js
@@ -68,6 +68,7 @@ export async function getStaticProps({ params, preview, previewData }) {
     data && (data.preload = true); // set this flag so we know the data is preloaded
   } catch (error) {}
 
+  console.log("rendering site: ", siteName);
   return {
     props: {
       sitePath: siteName,
