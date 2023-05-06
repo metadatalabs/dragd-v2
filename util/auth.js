e9b5960760528c8db230f0ba836e32f5e0a4cd5a
prnth <pranith_h@yahoo.com.sg>
2023-05-06 21:16:52 +0530

    switch to graph protocol for showing ENS names

--- util/auth.js
+++ util/auth.js
@@ -22,29 +22,34 @@ let url = "https://rpc.flashbots.net";
 let provider = new ethers.providers.JsonRpcProvider(url);
 
 const whitelistedAccountDomains = {
-  "prnth.eth": ["index", "docs.eth", "loveposters.eth", "nounshack.eth"],
-  "0xlok.eth": ["l.eth", "loveposters.eth"],
-  "salmanneedsajob.eth": ["nounshack.eth"],
+  "0x88289ac519eFb1cba5F522970E63264a969BeD06": [
+    "index",
+    "docs.eth",
+    "loveposters.eth",
+    "nounshack.eth",
+  ],
+  "0xE5E98Df807c3C4F8e57eeeED0968895b2EA5FEfb": ["l.eth", "loveposters.eth"],
+  "0x4DF83971f6f1bFD8D33a2E79584bDFDe75F4DF60": ["nounshack.eth"],
 };
 export const getBlockchainNames = async (address) => {
   // const res = await fetch(`https://api.opensea.io/api/v1/assets?owner=${address}&order_direction=desc&offset=0&limit=20`);
   // const json = await res.json();
   // return json.assets.map(asset => asset.name);
   var addresses = [address];
-  let ensName;
+  let ensNames;
 
   if (address && ethers.utils.isAddress(address)) {
     try {
-      ensName = await getData(address);
+      ensNames = await getData(address);
     } catch (e) {
       console.log(e);
     }
   }
 
-  if (ensName) addresses.push(ensName);
+  if (ensNames) addresses = [...addresses, ...ensNames];
 
-  if (ensName && Object.keys(whitelistedAccountDomains).includes(ensName)) {
-    addresses = [...addresses, ...whitelistedAccountDomains[ensName]];
+  if (address && Object.keys(whitelistedAccountDomains).includes(address)) {
+    addresses = [...addresses, ...whitelistedAccountDomains[address]];
   }
 
   return addresses;
@@ -77,7 +82,45 @@ function setCachedData(key, data) {
 
 async function getDataFromApi(address) {
   // implement the code to get data from the API
-  return await provider.lookupAddress(address);
+
+  // const defaultName = await provider.lookupAddress(address);
+  const graphNames = await getNamesFromGraph(address);
+  return [graphNames];
+}
+
+async function getNamesFromGraph(address) {
+  const graphNames = await fetch(
+    "https://api.thegraph.com/subgraphs/name/ensdomains/ens",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+        {
+          accounts(where: {id: "${address.toLowerCase()}"}) {
+            wrappedDomains {
+              name
+            }
+            domains {
+              name
+            }
+          }
+        }
+        `,
+      }),
+    }
+  );
+  const res = (await graphNames.json()).data.accounts[0];
+
+  var domains = [];
+  if (res?.domains?.length > 0)
+    res.domains.map((domain) => domains.push(domain.name));
+  if (res?.wrappedDomains?.length > 0)
+    res.wrappedDomains.map((domain) => domains.push(domain.name));
+
+  return domains;
 }
 
 async function getData(key) {
