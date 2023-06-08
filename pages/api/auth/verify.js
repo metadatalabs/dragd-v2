ec4e039ecbd6ba03406c836d40d6755eb433a66b
prnth <pranith_h@yahoo.com.sg>
2023-06-08 11:21:37 +0530

    added user table

--- pages/api/auth/verify.js
+++ pages/api/auth/verify.js
@@ -1,34 +1,45 @@
-import { withSessionRoute } from '/util/auth'
-import { SiweMessage } from 'siwe'
+import { createUser, getUserByWalletId, updateUser } from "../_db";
+import { withSessionRoute } from "/util/auth";
+import { SiweMessage } from "siwe";
 
 const handler = async (req, res) => {
-  const { method } = req
+  const { method } = req;
   switch (method) {
-    case 'POST':
+    case "POST":
       try {
-        const { message, signature } = req.body
-        const siweMessage = new SiweMessage(message)
-        const verificationResult = await siweMessage.verify({signature})
+        const { message, signature } = req.body;
+        const siweMessage = new SiweMessage(message);
+        const verificationResult = await siweMessage.verify({ signature });
 
-        if(!verificationResult.success)
-          return res.status(422).json({ message: 'Invalid signature.' })
+        if (!verificationResult.success)
+          return res.status(422).json({ message: "Invalid signature." });
         if (siweMessage.nonce !== req.session.nonce)
-          return res.status(422).json({ message: 'Invalid nonce.' })
- 
+          return res.status(422).json({ message: "Invalid nonce." });
 
-        console.log("logging in user with walletId: ", verificationResult.data.address)
-        req.session.siwe = verificationResult.data
-        await req.session.save()
+        console.log(
+          "logging in user with walletId: ",
+          verificationResult.data.address
+        );
+        req.session.siwe = verificationResult.data;
+        await req.session.save();
 
-        res.json({ ok: true })
+        res.json({ ok: true });
+
+        // update user table
+        var users = await getUserByWalletId(verificationResult.data.address);
+        if (users.length > 0) {
+          updateUser(users[0].uid, { lastSeen: Date.now() });
+        } else {
+          createUser(verificationResult.data.address);
+        }
       } catch (_error) {
-        res.json({ ok: false })
+        res.json({ ok: false });
       }
-      break
+      break;
     default:
-      res.setHeader('Allow', ['POST'])
-      res.status(405).end(`Method ${method} Not Allowed`)
+      res.setHeader("Allow", ["POST"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
   }
-}
- 
+};
+
 export default withSessionRoute(handler);
